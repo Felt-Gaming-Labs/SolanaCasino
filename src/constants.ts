c4e7c91a98dc3a39a5c9b7515e69c218e846842c
Felt Gaming Labs <team@feltgaming.io>
2024-02-05 23:17:17 -0800

    Resolved conflict in .env

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { GambaStandardTokens, TokenMeta } from 'gamba-react-ui-v2'
 import { PublicKey } from '@solana/web3.js'
 
 // Can be configured in .env
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com"
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? "https://mainnet.helius-rpc.com/?api-key=a5da6914-88b4-4f38-8b27-6a16bf71892b"
 
 // Change this value to your Solana address
 export const PLATFORM_CREATOR_ADDRESS = 'DgJzvFwdPvgRJkST27Pf6KSEbWwotKjLH3QyTKt3a5dj'
